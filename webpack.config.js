f211264ee9053a748cafd8dcda3e34372e2065e2
nyydmb12 <danielwest13@outlook.com>
2018-07-14 09:30:29 -0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : false,
+  entry: "./js/client.js",
+ //devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-0'],
+            plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+          }
+        }
+      }
+    ]
+  }
+  ,
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+};
